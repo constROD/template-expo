e508235eecd1fc17c1b7c0977a7fe900019de37b
constROD <rialcantara21@gmail.com>
2024-09-14 18:51:54 +0800

    chore: add default api url

--- src/env.ts
+++ src/env.ts
@@ -4,10 +4,12 @@ import { STAGES } from './constants/env';
 
 const envSchema = z.object({
   STAGE: z.nativeEnum(STAGES).default(STAGES.Dev),
+  API_URL: z.string(),
 });
 
 export const envConfig = envSchema.parse({
   STAGE: process.env.EXPO_PUBLIC_STAGE,
+  API_URL: process.env.EXPO_PUBLIC_API_URL,
 });
 
 export type EnvConfig = z.infer<typeof envSchema>;
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,16 +1,11 @@
 import axios from 'axios';
-import Constants from 'expo-constants';
 
+import { envConfig } from '@/env';
 import { useSessionStore } from '@/hooks/stores/use-session-store';
-
-const getBaseUrl = () => {
-  const localhost = Constants.expoConfig?.hostUri?.split(':')[0] ?? 'localhost';
-  return __DEV__ ? `http://${localhost}:3000` : /* envConfig.API_URL */ '';
-};
 
 const publicAxiosInstance = axios.create();
 
-const privateAxiosInstance = axios.create({ baseURL: getBaseUrl() });
+const privateAxiosInstance = axios.create({ baseURL: envConfig.API_URL });
 
 privateAxiosInstance.interceptors.request.use(request => {
   request.headers.Authorization = `Bearer ${useSessionStore.getState().session?.access_token}`;

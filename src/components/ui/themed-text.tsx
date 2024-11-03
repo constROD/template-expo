d3c77ea72638248f8213b2c115f7f57f48e336e5
constROD <rialcantara21@gmail.com>
2024-11-04 03:43:59 +0800

    chore: update themed text color

--- src/components/ui/themed-text.tsx
+++ src/components/ui/themed-text.tsx
@@ -1,9 +1,9 @@
 import { Text, type TextProps } from 'react-native';
 
-import { FONTS } from '@/constants/theme';
+import { COLORS, FONTS } from '@/constants/theme';
 
 export type ThemedTextProps = TextProps;
 
 export function ThemedText({ ...props }: ThemedTextProps) {
-  return <Text {...props} style={[FONTS.regular, props.style]} />;
+  return <Text {...props} style={[{ ...FONTS.regular, color: COLORS.foreground }, props.style]} />;
 }
